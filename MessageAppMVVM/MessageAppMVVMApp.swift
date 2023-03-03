1e1f109327584b0cc2810900b57f4cc25028bbe3
hide0101 <hideakikomori0101@gmail.com>
2023-03-03 18:25:20 +0900

    Initial Commit

---
+++ MessageAppMVVM/MessageAppMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageAppMVVMApp.swift
+//  MessageAppMVVM
+//
+//  Created by hideaki komori on 2023/03/03.
+//
+
+import SwiftUI
+
+@main
+struct MessageAppMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
