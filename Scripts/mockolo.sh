f03dead334960df1bc41675636b6142901d617dd
hide0101 <hideakikomori0101@gmail.com>
2023-03-16 00:42:09 +0900

    Add generate repository mock script

--- Scripts/mockolo.sh
+++ Scripts/mockolo.sh
@@ -2,6 +2,8 @@
 if which mint >/dev/null; then
     # UseCase
     mint run mockolo --sourcedirs $SRCROOT/MessageAppMVVM/Model/UseCase/ --destination $SRCROOT/MessageAppMVVM/Model/UseCase/Mock/UseCaseMock.swift
+    # Repository
+    mint run mockolo --sourcedirs $SRCROOT/MessageAppMVVM/Model/Repository/ --destination $SRCROOT/MessageAppMVVM/Model/Repository/Mock/RepositoryMock.swift
 else
     echo 'warning: Mint not installed. Please install mint from https://github.com/yonaskolb/Mint'
 fi
