56eba2160c9629fbf0032f4378cc26d5b487dd97
hide0101 <hideakikomori0101@gmail.com>
2023-03-26 15:58:13 +0900

    Implement SignInView

---
+++ MessageAppMVVM/View/SignInView.swift
@@ -0,0 +1,40 @@
+//
+//  SignInView.swift
+//  MessageAppMVVM
+//
+//  Created by hide0101 on 2023/03/26.
+//
+
+import SwiftUI
+
+/// サインイン画面
+internal struct SignInView: View {
+    /// メールアドレス
+    @State private var email = ""
+    /// パスワード
+    @State private var password = ""
+    /// body
+    internal var body: some View {
+        VStack {
+            Text("ここでエラーを表示する")
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text("パスワード")
+                .frame(maxWidth: .infinity, alignment: .leading)
+            TextField("入力してください", text: $email)
+            Text("ここでエラーを表示する")
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Button("サインイン") {
+                // TODO 登録メソッドを実行する
+                print("サインインメソッドを実行する")
+            }
+        }
+    }
+}
+
+/// サインイン画面のPreviews
+internal struct SignInView_Previews: PreviewProvider {
+    /// previews
+    internal static var previews: some View {
+        SignInView()
+    }
+}
