ceac69cdabfb5c2630cef52be7977d8903760815
hide0101 <hideakikomori0101@gmail.com>
2023-03-28 01:59:03 +0900

    Implement AuthError

--- MessageAppMVVM/Model/AuthError.swift
+++ MessageAppMVVM/Model/AuthError.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+/// 認証のエラー種別
+internal enum AuthError: Error {
+    /// メールアドレスの形式が正しくない
+    case invalidEmail
+    /// メールアドレスがすでに使用されている
+    case emailAlreadyInUse
+    /// パスワードの強度が弱い
+    case weakPasssword
+    /// 予期せぬエラー
+    case unexpected
+}
