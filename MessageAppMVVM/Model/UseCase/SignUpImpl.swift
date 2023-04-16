6759a26c44ae6b186f0668d4e1c962dbc8c7e6bd
hide0101 <hideakikomori0101@gmail.com>
2023-04-16 22:04:51 +0900

    Fix swiftlint warning

--- MessageAppMVVM/Model/UseCase/SignUpImpl.swift
+++ MessageAppMVVM/Model/UseCase/SignUpImpl.swift
@@ -10,63 +10,68 @@ import FirebaseAuth
 import FirebaseFirestore
 import Foundation
 
+/// SignUpを実行するUseCase
 internal struct SignUpImpl: SignUp {
-    
+    /// AuthRepository
     @Inject private var authRepository: AuthRepository
+    /// DBRepository
     @Inject private var dbRepository: DBRepository
-    
-    internal func execute(name: String, email: String, password: String) -> AnyPublisher<ResultData<SignUpStatus>, Never> {
+    /// 実行
+    /// - Parameters:
+    ///     - name: ユーザー名
+    ///     - email: メールアドレス
+    ///     - password: パスワード
+    /// - Returns:
+    ///     ResultData
+    internal func execute(
+        name: String,
+        email: String,
+        password: String
+    ) -> AnyPublisher<ResultData<SignUpStatus>, Never> {
         authRepository.createUserWithEmailAndPassword(email: email, password: password)
             .mapError { $0 as Error }
             .flatMap { result -> AnyPublisher<Void, Error> in
                 if let result = result {
                     return dbRepository.createUser(uid: result.user.uid, name: name)
+                        .mapError { $0 as Error }
+                        .eraseToAnyPublisher()
                 }
                 return Fail(error: AuthError.unexpected).eraseToAnyPublisher()
             }
-            .map { result -> ResultData<SignUpStatus> in
-                return ResultData(status: SignUpStatus.success)
+            .map { _ -> ResultData<SignUpStatus> in
+                ResultData(status: SignUpStatus.success)
             }
             .catch({ error -> AnyPublisher<ResultData<SignUpStatus>, Never> in
-                switch error  {
-                case let error as AuthError:
-                    if case .invalidEmail = error {
-                        return Just(ResultData(status: SignUpStatus.emailAlReadyInUse)).eraseToAnyPublisher()
-                    }
-                    if case .emailAlreadyInUse = error {
-                        return Just(ResultData(status: SignUpStatus.weakPassword)).eraseToAnyPublisher()
-                    }
-                    if case .weakPasssword = error {
-                        return Just(ResultData(status: SignUpStatus.weakPassword)).eraseToAnyPublisher()
-                    }
-                    if case .unexpected = error {
-                        return Just(ResultData(status: SignUpStatus.unexpectedError)).eraseToAnyPublisher()
-                    }
-                default:
-                    return Just(ResultData(status: SignUpStatus.networkError)).eraseToAnyPublisher()
-                }
-                return Just(ResultData(status: SignUpStatus.networkError)).eraseToAnyPublisher()
+                Just(handleError(error: error)).eraseToAnyPublisher()
             })
             .eraseToAnyPublisher()
     }
-    
-}
 
-internal struct ResultData<T> {
-    internal let status: T
-    internal let data: Codable?
-    
-    internal init(status: T, data: Codable? = nil) {
-        self.status = status
-        self.data = data
+    /// エラーを識別し該当するResultDataを返却する
+    /// - Parameters:
+    ///     - error: エラー
+    /// - Returns:
+    ///     ResultData
+    private func handleError(error: Error) -> ResultData<SignUpStatus> {
+        switch error {
+        case let error as AuthError:
+            if case .invalidEmail = error {
+                return ResultData(status: SignUpStatus.invalidEmail)
+            } else if case .emailAlreadyInUse = error {
+                return ResultData(status: SignUpStatus.emailAlReadyInUse)
+            } else if case .weakPasssword = error {
+                return ResultData(status: SignUpStatus.weakPassword)
+            } else if case .unexpected = error {
+                return ResultData(status: SignUpStatus.unexpectedError)
+            }
+            return ResultData(status: SignUpStatus.networkError)
+        case let error as DBError:
+            if case .unexpected = error {
+                return ResultData(status: SignUpStatus.unexpectedError)
+            }
+            return ResultData(status: SignUpStatus.networkError)
+        default:
+            return ResultData(status: SignUpStatus.unexpectedError)
+        }
     }
-}
-
-internal enum SignUpStatus {
-    case success
-    case networkError
-    case emailAlReadyInUse
-    case weakPassword
-    case invalidEmail
-    case unexpectedError
 }
