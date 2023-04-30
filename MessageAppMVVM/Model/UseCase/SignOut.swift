2f57510b54b975a7705d43d9b832334e5cf27959
hide0101 <hideakikomori0101@gmail.com>
2023-04-30 22:24:03 +0900

    Implement SignOutStatus

---
+++ MessageAppMVVM/Model/UseCase/SignOut.swift
@@ -0,0 +1,8 @@
+//
+//  SignOut.swift
+//  MessageAppMVVM
+//
+//  Created by hide0101 on 2023/04/30.
+//
+
+import Foundation
---
+++ MessageAppMVVM/Model/UseCase/SignOutStatus.swift
@@ -0,0 +1,13 @@
+//
+//  SignOutStatus.swift
+//  MessageAppMVVM
+//
+//  Created by hide0101 on 2023/04/30.
+//
+
+import Foundation
+
+internal enum SignOutStatus {
+    case success
+    case unexpectedError
+}
