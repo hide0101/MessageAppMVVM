9d8d8df7b174bce2712b459a5019ec231bdfda83
hide0101 <hideakikomori0101@gmail.com>
2023-04-30 22:24:17 +0900

    Implement SignOut

--- MessageAppMVVM/Model/UseCase/SignOut.swift
+++ MessageAppMVVM/Model/UseCase/SignOut.swift
@@ -5,4 +5,11 @@
 //  Created by hide0101 on 2023/04/30.
 //
 
+import Combine
 import Foundation
+
+/// サインアウトするUseCase
+internal protocol SignOut {
+    /// 実行
+    func execute() -> AnyPublisher<ResultData<SignOutStatus>, Never>
+}
